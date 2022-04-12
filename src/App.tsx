43b14c471540b6f0822b5f76ec763deb6a423a8a
Luka Košćak <koscak.luka@gmail.com>
2022-04-12 15:36:45 +0100

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import * as React from "react";
+import "./App.css";
+
+interface AppProps {}
+
+interface AppState {}
+
+class App extends React.Component<AppProps, AppState> {
+  //state = { :  }
+  render() {
+    return "";
+  }
+}
+
+export default App;
